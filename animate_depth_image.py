30ed572c7d422bd2aee20aab4b1c33d7e33af3a8
Woo Chia Wei <chiaweiwoo123@gmail.com>
2017-10-18 11:28:04 +0800

    Code refactoring on animation function.

--- animate_depth_image.py
+++ animate_depth_image.py
@@ -1,35 +1,45 @@
+import sys
 import scipy.io
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from pathlib import Path
 
-# Configure action, subject and trial
-action = 18 # Range from 1 to 27
-subject = 6 # Range from 1 to 8
-trial = 1 # Range from 1 to 4
+def animate_depth_data(action, subject, trial):
+    # To update the data being passed to the next plot 
+    def update_fig(*args):
+        nonlocal frame
+        if frame == data.shape[2]: frame = 0
+        im.set_array(data[:,:,frame])
+        frame = frame + 1
+        return im,
 
-# To update the data being passed to the next plot 
-def animate(*args):
-    global frame
-    if frame == data.shape[2]: frame = 0
-    im.set_array(data[:,:,frame])
-    frame = frame + 1
-    return im,
+    # Initialization 
+    frame = 0
+    fig = plt.figure()
 
-# Initialization 
-frame = 0
-fig = plt.figure()
+    # Check if file exists. If file is not found, exit the program.
+    filename = f'data/Depth/a{action}_s{subject}_t{trial}_depth.mat'
+    if Path(filename).is_file():
+        mat = scipy.io.loadmat(filename)
+        data = mat['d_depth']
+    else:
+        print(f'Error: Kinect file {filename} is not found.')
+        sys.exit()
 
-# Check if file exists. If file is not found, exit the program.
-filename = f'data/Depth/a{action}_s{subject}_t{trial}_depth.mat'
-if Path(filename).is_file():
-    mat = scipy.io.loadmat(filename)
-    data = mat['d_depth']
-else:
-    print(f'File {filename} is not found!.')
-    sys.exit()
+    # Create animated plot 
+    im = plt.imshow(data[:,:,0], animated=True, cmap='gray')
+    ani = animation.FuncAnimation(fig, update_fig, interval=33, blit=True)
+    plt.show()
 
-# Create animated plot 
-im = plt.imshow(data[:,:,0], animated=True, cmap='gray')
-ani = animation.FuncAnimation(fig, animate, interval=33, blit=True)
-plt.show()
+if __name__ == "__main__":
+    if(len(sys.argv) != 4):
+        print("Error: 3 parameters are expected.")
+        sys.exit()
+    try:
+        action = int(sys.argv[1]) # ranged from 1 to 27
+        subject = int(sys.argv[2]) # ranged from 1 to 8
+        trial = int(sys.argv[3]) # ranged from 1 to 4
+    except ValueError:
+        print("Error: Integer parameter is expected.")
+        sys.exit()
+    animate_depth_data(action, subject, trial)
